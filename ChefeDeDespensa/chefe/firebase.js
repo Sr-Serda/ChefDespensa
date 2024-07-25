98b8783a901d0f14dbe867b3417a3991f68c2f6a
Gustavo <gustavomoreirasilvaoficial@gmail.com>
2024-07-25 13:38:21 -0300

    Update firebase.js

--- ChefeDeDespensa/chefe/firebase.js
+++ ChefeDeDespensa/chefe/firebase.js
@@ -2,14 +2,7 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBd-GZw1I9B-Vbn6t8WEovsx8303cTXCk4",
-  authDomain: "chefe-685b2.firebaseapp.com",
-  databaseURL: "https://chefe-685b2-default-rtdb.firebaseio.com",
-  projectId: "chefe-685b2",
-  storageBucket: "chefe-685b2.appspot.com",
-  messagingSenderId: "458923499304",
-  appId: "1:458923499304:web:b13723caf84c2c4c55fa1d",
-  measurementId: "G-GV3RLHWBVV",
+ <"YOUR API FIREBASECONFIG">
 };
 
 // Initialize Firebase
